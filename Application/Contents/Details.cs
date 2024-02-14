789f5104cdf96923140400533ea0e0e4d36f2e25
heng somnang <hsomnang098@gmail.com>
2024-02-14 11:48:11 +0700

    details content api

---
+++ Application/Contents/Details.cs
@@ -0,0 +1,29 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Contents
+{
+    public class Details
+    {
+        public class Query : IRequest<Content>
+        {
+            public int Id { get; set; }
+            public class Handler : IRequestHandler<Query, Content>
+            {
+                private readonly DataContext _context;
+                public Handler(DataContext context)
+                {
+                    _context = context;
+                }
+
+                public async Task<Content> Handle(Query request, CancellationToken cancellationToken)
+                {
+                    return await _context.Contents.FindAsync(request.Id);
+                }
+            }
+        }
+    }
+}
--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db304e40f4bb0ba54f956520206a3adb2ecf693f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
